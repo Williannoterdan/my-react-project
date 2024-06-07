3838a37a9c1f654fc6b1e1028aa182241bb2a867
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-06-07 15:01:46 +0300

    апрап

---
+++ src/components/Alert.jsx
@@ -0,0 +1,12 @@
+// src/components/Alert.jsx
+import "./Alert.css";
+
+
+
+
+
+export const Alert = ({ variant, children }) => {
+  const classNames = ["alert", variant];
+
+  return <p className={classNames.join(" ")}>{children}</p>;
+};
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,24 @@
 
+import PropTypes from 'prop-types';
 
-export const Product = ({
-  name,
-  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
-  price,
+const Product = ({
+ name,
+ imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+ price,
 }) => (
-  <div>
+ <div>
+		<img src={imgUrl} alt={name} width="480" />
 		<h2>{name}</h2>
-    <img src={imgUrl} alt={name} width="480" />
-    <p>Price: {price} credits</p>
-  </div>
+ <p>Price: {price} credits</p>
+ </div>
 );
+
+Product.propTypes = {
+ name: PropTypes.string.isRequired,
+ imgUrl: PropTypes.string,
+ price: PropTypes.number.isRequired,
+};
+
+export default Product;
 
 

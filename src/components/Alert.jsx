f63e50d5f63dd919c656637d887f6ba9ae9a429c
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-06-12 04:27:01 +0300

    укукцк

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,43 @@
 // src/components/Alert.jsx
 import "./Alert.css";
-
+// import clsx from "clsx";
+import css from "./Alert.module.css";
 
 
 
 
 export const Alert = ({ variant, children }) => {
-  const classNames = ["alert", variant];
-
-  return <p className={classNames.join(" ")}>{children}</p>;
+  return <p className={css[variant]}>{children}</p>;
 };
+
+
+
+// export const Alert = ({ variant, children }) => {
+//   const classNames = ["alert", variant];
+
+//   return <p className={classNames.join(" ")}>{children}</p>;
+// };
+
+
+
+// export const Alert = ({ variant, outlined, elevated, children }) => {
+//   return (
+//     <p
+//       className={clsx("alert", variant, {
+//         "is-outlined": outlined,
+//         "is-elevated": elevated,
+//       })}
+//     >
+//       {children}
+//     </p>
+//   );
+// };
+
+
+
+// Синтаксис імпорту CSS-модуля
+
+
+// Отримуємо об'єкт відповідності імен класів
+console.log(css); // { alert: "Alert_alert_ax7yz" }
+
